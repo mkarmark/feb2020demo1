d904a5834cc70e5b99b58d3780eca5b98af651d5
Mitali Karmarkar <mikarmar@microsoft.com>
2021-02-24 10:59:51 -0800

    please now

--- api/GetTopTVShows2020/index.js
+++ api/GetTopTVShows2020/index.js
@@ -6,16 +6,13 @@ module.exports = async function (context, req) {
     const topTVShows2020 = {
         "results": [
             {
-                "title": "The Crown",
-                "image": "https://specials-images.forbesimg.com/imageserve/5f43ed103cbce1abbd96f810/960x0.jpg?cropX1=0&cropX2=1080&cropY1=238&cropY2=1080"
+                "title": "The Crown"
             },
             {
-                "title": "Money Heist",
-                "image": "https://filmdaily.co/wp-content/uploads/2020/05/Money-Heist-S4-lede.jpg"
+                "title": "Money Heist"
             },
             {
-                "title": "Schitt's Creek",
-                "image": "https://m.media-amazon.com/images/M/MV5BNWQ1ZmM3MTQtNTVhZC00MWVlLWI5ZjgtYmZiYWQxZjUzZWM0XkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_.jpg"
+                "title": "Schitt's Creek"
             }
         ]
     }
