ab9e49206a5f8fc66ede406568cf80c4c988b746
Mitali Karmarkar <mikarmar@microsoft.com>
2021-02-24 15:51:33 -0800

    fix site

--- api/GetTopTVShows2020/index.js
+++ api/GetTopTVShows2020/index.js
@@ -1,33 +1,38 @@
 var fs = require('fs');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    const connectionString = process.env.STORAGE_CONNECTION_STRING;
+    const containerName = process.env.STORAGE_CONTAINER;
+    const accountName = process.env.STORAGE_ACCOUNT;
 
-    var path = context.executionContext.functionDirectory + '/movies.json';
-    var jsonData = path;
-    var error = "";
-    fs.readFile(path, 'utf8', function (err, data) {
-        if (err) {
-            error = err;
-        }
+    // create a new instance of blogServiceClient from the Azure Storage SDK
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+        connectionString
+    );
 
-        jsonData = data;
-    });
-    const topTVShows2020 = {
-        "results": [
-            {
-                "title": "The Crown"
-            },
-            {
-                "title": "Money Heist"
-            },
-            {
-                "title": "Schitt's Creek"
-            }
-        ]
+    // create a new instance of the containerClient from the Azure Storage SDK
+    const containerClient = await blobServiceClient.getContainerClient(
+        containerName
+    );
+
+    const blobs = [];
+
+    // list all objects in the storage container
+    for await (const blob of containerClient.listBlobsFlat({
+        includeMetadata: true,
+    })) {
+        blobs.push(blob);
     }
 
+    // order the blobs by date descending
+    blobs.sort((a, b) => {
+        return (
+        new Date(b.properties.lastModified) - new Date(a.properties.lastModified)
+        );
+    });
+
     context.res = {
-        body: jsonData + error
-    } 
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(blobs)
+    };
 };
