ce048d5f0291f539b3fcba436ca480e88e93f1d6
Mitali Karmarkar <mikarmar@microsoft.com>
2021-02-24 14:26:03 -0800

    try now

--- api/GetTopTVShows2020/index.js
+++ api/GetTopTVShows2020/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var path = context.executionContext.functionDirectory + '//movies.json';
-    var jsonData = "";
+    var jsonData = context.executionContext.functionDirectory;
     fs.readFile(path, 'utf8', function (err, data) {
         if (err) {
             context.log.error(err);
