8503952edebca58bc3b68ea40af2a61805726e82
Mitali Karmarkar <mikarmar@microsoft.com>
2021-02-24 14:29:14 -0800

    try now

--- api/GetTopTVShows2020/index.js
+++ api/GetTopTVShows2020/index.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    var path = context.executionContext.functionDirectory + '//movies.json';
-    var jsonData = context.executionContext.functionDirectory;
+    var path = context.executionContext.functionDirectory + '/movies.json';
+    var jsonData = "";
     fs.readFile(path, 'utf8', function (err, data) {
         if (err) {
             context.log.error(err);
