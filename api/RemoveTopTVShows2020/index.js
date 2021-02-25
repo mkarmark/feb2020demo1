72bd53856788bc99c8c666b26ccd4fc5e62dacd1
Mitali Karmarkar <mikarmar@microsoft.com>
2021-02-24 20:23:28 -0800

    add delete function

---
+++ api/RemoveTopTVShows2020/index.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+module.exports = async function (context, req) {
+    const connectionString = process.env.STORAGE_CONNECTION_STRING;
+    const containerName = process.env.STORAGE_CONTAINER;
+    const accountName = process.env.STORAGE_ACCOUNT;
+
+    var blobName = req.query.blobName
+
+    // create a new instance of blogServiceClient from the Azure Storage SDK
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+        connectionString
+    );
+
+    // create a new instance of the containerClient from the Azure Storage SDK
+    const containerClient = await blobServiceClient.getContainerClient(
+        containerName
+    );
+
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const deleteBlobResponse = await blockBlobClient.deleteBlob(blobStringToAdd);
+
+    context.res = {
+        status: deleteBlobResponse.status
+    };
+};
