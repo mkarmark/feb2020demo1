a717d1f6aa3b87a15481e0881ff5c6932e7f64a5
Mitali Karmarkar <mikarmar@microsoft.com>
2021-02-24 22:23:24 -0800

    try to update react

--- app/src/TopTVShows2020.js
+++ app/src/TopTVShows2020.js
@@ -101,7 +101,7 @@ class TopTVShows2020 extends React.Component {
 			)
 		)
 		}
-    {this.state.showAddInput ? (
+    {true ? (
     <form onSubmit={this.handleAddSubmit}>
       <label>
         TV Show Name:
