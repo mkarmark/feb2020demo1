31b8a3f99ceb4bdabc95fc616ecf281e78443304
Mitali Karmarkar <mikarmar@microsoft.com>
2021-02-24 20:08:17 -0800

    try new function

---
+++ api/AddTopTVShows2020/index.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+module.exports = async function (context, req) {
+    const connectionString = process.env.STORAGE_CONNECTION_STRING;
+    const containerName = process.env.STORAGE_CONTAINER;
+    const accountName = process.env.STORAGE_ACCOUNT;
+
+    var blobName = req.query.blobName
+    var blobStringToAdd = JSON.stringify(req.body);
+
+    // create a new instance of blogServiceClient from the Azure Storage SDK
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+        connectionString
+    );
+
+    // create a new instance of the containerClient from the Azure Storage SDK
+    const containerClient = await blobServiceClient.getContainerClient(
+        containerName
+    );
+
+    const blobClient = containerClient.getBlobClient(blobName);
+    const uploadBlobResponse = await blobClient.upload(blobStringToAdd, blobStringToAdd.length);
+
+    context.res = {
+        status: uploadBlobResponse.status
+    };
+};
